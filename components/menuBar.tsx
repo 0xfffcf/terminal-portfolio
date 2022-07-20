6c8f4b2468d6a1b901abf49abdba549c268f4de7
Sofia <sofia@hi.xyz>
2022-07-20 19:25:28 -0400

    feat: Add Icon To menu bar

--- components/menuBar.tsx
+++ components/menuBar.tsx
@@ -1,5 +1,18 @@
-const MenuBar: React.FC = () => {
+import { Box } from "@chakra-ui/react"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
+
+import styles from '../styles/MenuBar.module.scss';
+
+const MenuBar: React.FC = () => {
+    return (
+        <Box pos={'absolute'} w={'100vw'} h={'5em'}>
+            <Box pt={'2em'} pl={'2em'}>
+                <FontAwesomeIcon icon={faMoon} size='2x' className={styles.icon}/>
+            </Box>
+        </Box>
+    )
 }
 
 export default MenuBar
