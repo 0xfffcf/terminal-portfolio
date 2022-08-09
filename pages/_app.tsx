b793e505be1b084ae4e05309b39058ae0d0ab43b
Reaper <yougothacked@protonmail.com>
2022-08-08 21:47:51 -0400

    refractor: change path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
-import '../styles/globals.scss'
+import '@/styles/globals.scss'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import theme from '../utils/theme'
+import theme from 'utils/theme'
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,16 @@
-import { ColorModeScript } from "@chakra-ui/react";
-import { Head, Html, Main, NextScript } from "next/document";
-import theme from "../utils/theme";
+import { ColorModeScript } from '@chakra-ui/react';
+import { Head, Html, Main, NextScript } from 'next/document';
+import theme from 'utils/theme';
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head/>
-            <body>
-                <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-                <Main/>
-                <NextScript/>
-            </body>
-        </Html>
-    )
+  return (
+    <Html lang='en'>
+      <Head />
+      <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
