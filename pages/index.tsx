9fa7322a91f791e326c69dec191af10889362874
Reaper <yougothacked@protonmail.com>
2022-08-08 21:46:58 -0400

    feat: add footer, etc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,36 @@
-import { Box } from '@chakra-ui/react'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import MenuBar from '../components/menuBar'
-import Terminal from '../components/terminal'
+import { Box, Center, Flex, Text } from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Terminal from 'features/terminal';
 
 const Home: NextPage = () => {
   return (
     <Box h={'100vh'}>
       <Head>
         <title>0xfffcf</title>
-        <meta charSet='utf-8'/>
-        <meta name="description" content="Hi, I am 0xfffcf a web developer. I am currenlty working with Angular, NextJs and more!"/>
-        <meta name="keywords" content="portfolio, web designer, web developer, fullstack"/>
-        <meta name="author" content="0xfffcf"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta charSet='utf-8' />
+        <meta
+          name='description'
+          content='Hi, I am 0xfffcf a web developer. I am currenlty working with Angular, NextJs and more!'
+        />
+        <meta
+          name='keywords'
+          content='portfolio, web designer, web developer, fullstack'
+        />
+        <meta name='author' content='0xfffcf' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
-      <MenuBar/>
-      <Terminal/>
+      <Terminal />
+      <Flex justifyContent='center'>
+        <Text pos='absolute' bottom='5' m='auto'>
+          Made with ❤️ by{' '}
+          <Text as='span' color='blue.400'>
+            <a href='https://github.com/0xfffcf'>0xfffcf</a>
+          </Text>
+        </Text>
+      </Flex>
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
