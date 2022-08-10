eb8b0800356076581e3c1e8be37ceeed3a81f624
Reaper <yougothacked@protonmail.com>
2022-08-09 22:03:47 -0400

    feat: add responsive

--- features/footer.tsx
+++ features/footer.tsx
@@ -1,13 +1,16 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, useMediaQuery } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 
 const Footer: React.FC = () => {
+  const [isCellPhoneLandScape] = useMediaQuery('(max-height: 600px)');
+
   return (
     <Flex
       justifyContent='center'
       as={motion.div}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
+      hidden={isCellPhoneLandScape}
     >
       <Text pos='absolute' bottom='5' m='auto'>
         Made with ❤️ by{' '}
