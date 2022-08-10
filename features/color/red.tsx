cf558f1fece72b1e40a27e7f06c6da0d09a0a7f2
Reaper <yougothacked@protonmail.com>
2022-08-09 20:47:00 -0400

    feat: create color components

---
+++ features/color/red.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react';
+import IText from './text';
+
+const Blue: Function = ({ text }: IText) => {
+  return (
+    <Text as='span' color='red.400'>
+      {text}
+    </Text>
+  );
+};
+
+export default Blue;
